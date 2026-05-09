6e59a45fd365caa17612ce8ebb4b9f4c91df851c
rohit2775 <rg7558674@gmail.com>
2026-05-09 22:59:51 +0530

    conditional rendering

--- vite-project/src/Product.jsx
+++ vite-project/src/Product.jsx
@@ -7,7 +7,9 @@ function Product ({title , price , features }){
     <div className="Product">
       <h3>  {title} </h3>
       <h3> {price} </h3>
-      <h3>  {features}</h3>
+      {price <3000 ? <p> discount available</p> : ""}
+      
+
     </div>
   )
 }
--- vite-project/src/ProductTab.jsx
+++ vite-project/src/ProductTab.jsx
@@ -5,11 +5,11 @@ import Product from "./Product"
 
 function ProductTab (){
    
-  let features = [<li>"hello"</li>,<li>"rohit"</li>,<li>"bhai"</li>] ;
+  
   
   return (
     <>
-     <Product title="phone" price={7678687} features={features} />
+     <Product title="phone" price={7678687} />
     <Product title="laptop" price={35433} />
      <Product title="tablets" price={2333} />
     </>
